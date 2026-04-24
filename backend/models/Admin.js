778877c0a133c448e221a4211a761aec7d63d3b4
swayamshinde <swayamhe123@gmail.com>
2026-04-24 19:43:51 +0530

    Add Admin model with password hashing

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const adminSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true }
+});
+
+adminSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
